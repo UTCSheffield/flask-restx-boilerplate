7238b577cc36aefa71f8c8826454296daafa3c30
Martyn Eggleton <martyn.eggleton@gmail.com>
2022-10-21 08:22:53 +0100

    Super simple serverside rendered page as a hub

---
+++ app/app_blueprint.py
@@ -0,0 +1,15 @@
+from flask import Blueprint
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
+from werkzeug.exceptions import abort
+
+app_blueprint = Blueprint('app_blueprint', __name__)
+
+@app_blueprint.route('/')
+def index():
+    return render_template("app/index.html")
+    
